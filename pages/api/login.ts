7c442292ce5512eafa2a2cc2cc95de18156c8b30
Vinicius Granado <vinicius.r.granado@gmail.com>
2021-06-16 10:56:02 -0300

    Added JWT login page

---
+++ pages/api/login.ts
@@ -0,0 +1,24 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+
+import { NextApiRequest, NextApiResponse } from "next"
+import jwt from 'jsonwebtoken';
+
+const KEY = 'asdfghjkl';
+
+export default (req: NextApiRequest, res: NextApiResponse) => {
+  if (!req.body) {
+    res.statusCode = 404;
+    res.end('Error');
+    return;
+  }
+
+  const { username, password } = req.body;
+
+  res.json({
+    token: jwt.sign({
+      username,
+      admin: username === 'admin' && password === 'admin',
+    },
+    KEY)
+  });
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,56 @@
+import { useState } from "react";
+import jwt from 'jsonwebtoken';
+ 
 export default function Home() {
+  const [username, setUsername] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+  const [message, setMessage] = useState<string>('You are not logged in');
+
+  const submitForm = async () => {
+    const res = await fetch('/api/login', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ username, password })
+    }).then((t) => t.json());
+
+    const token = res.token;
+
+    if (token) {
+      const json = jwt.decode(token) as { [key: string]: string };
+      console.log(json);
+
+      setMessage(`Welcome ${json.username}, you are ${json.admin ? 'an admin!' : 'not an admin'}`);
+    } else {
+      setMessage('Something went wrong');
+    }
+  }
+
   return (
-    <h1>Hello</h1>
+    <>
+      <h1>{message}</h1>
+      <form>
+        <input
+          type="text"
+          name='username'
+          value={username}
+          onChange={event => setUsername(event.target.value)}
+        />
+
+        <br/>
+
+        <input
+          type="password"
+          name='password'
+          value={password}
+          onChange={event => setPassword(event.target.value)}
+        />
+        
+        <br/>
+
+        <input type="button" value="Login" onClick={submitForm}/>
+      </form>
+    </>
   )
 }
